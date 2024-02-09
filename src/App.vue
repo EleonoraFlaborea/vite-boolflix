b93a6ad759f34eec33c2f3709dde824543ae176e
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-09 12:07:49 +0100

    add form and filter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script>
 import axios from 'axios';
+import { api } from './data/index.js';
 import { store } from './data/store.js';
+import SearchForm from './components/SearchForm.vue';
+
 
 export default {
   name: 'MyApp',
-  components: {},
+  data: () => ({ store }),
+  components: { SearchForm },
+  methods: {
+    searchMovies(titlefilter) {
+      console.log(titlefilter)
+    }
+  }
 }
 </script>
 
 <template>
-  <AppHeader />
+  <SearchForm placeholder="Scrivi..." buttonLabel="Cerca" />
 </template>
 
 <style lang="scss" scoped>
--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,9 +1,20 @@
 <script >
 export default {
     name: 'SearchForm',
+    props: {
+        buttonLabel: String,
+        placeholder: String,
+    },
+    emits: ['form-submit', 'term-change'],
 };
+
 </script>
 
-<template></template>
+<template>
+    <form @submit.prevent="$emit('form-submit', term)">
+        <input type="text" :placeholder="placeholder" v-model.trim="term" @keyup="$emit('term-change', term)">
+        <button>{{ buttonLabel }} </button>
+    </form>
+</template>
 
 <style></style>
