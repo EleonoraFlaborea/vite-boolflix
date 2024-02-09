bfb72bb1228cefb2d8dd5af382be4c0b5c6b6212
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-09 12:48:26 +0100

    add appMain, add h1, list movies

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
 import { store } from './data/store.js';
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
 
 
 export default {
   name: 'MyApp',
   data: () => ({ store }),
-  components: { AppHeader },
+  components: { AppHeader, AppMain },
 }
 
 </script>
 
 <template>
   <AppHeader />
+  <AppMain />
 </template>
 
 <style lang="scss" scoped>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,22 @@
 <script >
+import { store } from '../data/store.js';
 
+export default {
+    name: 'AppMain',
+    data: () => ({ store }),
+    components: {}
+}
 </script>
 
-<template></template>
+<template>
+    <main>
+        <section>
+            <h1>MOVIES</h1>
+            <ul v-for="movie in store.movies" :key="movie.id">
+                <li>{{ movie.title }}</li>
+            </ul>
+        </section>
+    </main>
+</template>
 
 <style></style>
